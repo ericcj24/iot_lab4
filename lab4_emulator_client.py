2c3b5f649be9e963b766e91295ed6fd1c5bbdb38
Jian C <jchen.lcy+dev@gmail.com>
2024-11-10 19:43:00 -0800

    step 1

---
+++ lab4_emulator_client.py
@@ -0,0 +1,109 @@
+# Import SDK packages
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import time
+import json
+import pandas as pd
+import numpy as np
+
+
+#TODO 1: modify the following parameters
+#Starting and end index, modify this
+device_st = 0
+device_end = 5
+device_name_formatter = 'picar_{}'
+
+#Path to the dataset, modify this
+data_path = "./vehicles/vehicle_{}.csv"
+
+#Path to your certificates, modify this
+certificate_formatter = "./certs/{}cert.pem"
+key_formatter = "./keys/{}private.key"
+
+
+class MQTTClient:
+    def __init__(self, device_id, cert, key):
+        # For certificate based connection
+        self.device_id = str(device_id)
+        self.state = 0
+        self.client = AWSIoTMQTTClient(self.device_id)
+        #TODO 2: modify your broker address
+        self.client.configureEndpoint("a376aankv8byup-ats.iot.us-east-2.amazonaws.com", 8883)
+        self.client.configureCredentials("./keys/AmazonRootCA1.pem", key, cert)
+        self.client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+        self.client.configureDrainingFrequency(2)  # Draining: 2 Hz
+        self.client.configureConnectDisconnectTimeout(10)  # 10 sec
+        self.client.configureMQTTOperationTimeout(5)  # 5 sec
+        self.client.onMessage = self.customOnMessage
+        
+
+    def customOnMessage(self,message):
+        #TODO 3: fill in the function to show your received message
+        print("client {} received payload {} from topic {}".format(self.device_id, message.payload, message.topic))
+
+
+    # Suback callback
+    def customSubackCallback(self,mid, data):
+        #You don't need to write anything here
+        pass
+
+
+    # Puback callback
+    def customPubackCallback(self,mid):
+        #You don't need to write anything here
+        pass
+
+
+    def publish(self, topic="vehicle/emission/data"):
+    # Load the vehicle's emission data
+        df = pd.read_csv(data_path.format(self.device_id))
+        for index, row in df.iterrows():
+            # Create a JSON payload from the row data
+            payload = json.dumps(row.to_dict())
+            
+            # Publish the payload to the specified topic
+            print(f"Publishing: {payload} to {topic}")
+            self.client.publishAsync(topic, payload, 0, ackCallback=self.customPubackCallback)
+            
+            # Sleep to simulate real-time data publishing
+            
+
+
+
+print("Loading vehicle data...")
+data = []
+for i in range(5):
+    device_id = device_name_formatter.format(i)
+    a = pd.read_csv(data_path.format(device_id))
+    data.append(a)
+
+print("Initializing MQTTClients...")
+clients = []
+for i in range(device_st, device_end):
+    device_id = device_name_formatter.format(i)
+    print("device id:", device_id)
+    client = MQTTClient(device_id,certificate_formatter.format(device_id,device_id) ,key_formatter.format(device_id,device_id))
+    client.client.connect()
+    clients.append(client)
+ 
+
+while True:
+    print("send now?")
+    x = input()
+    if x == "s":
+        for i,c in enumerate(clients):
+            c.publish()
+
+    elif x == "d":
+        for c in clients:
+            c.client.disconnect()
+        print("All devices disconnected")
+        exit()
+    else:
+        print("wrong key pressed")
+
+    time.sleep(3)
+
+
+
+
+
